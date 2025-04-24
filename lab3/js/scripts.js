7eee16a274d1ae1b132098b0d737ad091c56f49f
Artur Zacniewski <a.zacniewski@gmail.com>
2025-04-24 11:24:28 +0200

    Added files to lab. #3

--- lab3/js/scripts.js
+++ lab3/js/scripts.js
@@ -1,5 +1,32 @@
-function myFunction() {
-  alert ("Hello World!");
+function displayDate() {
+  document.getElementById("demoClick").innerHTML = Date();
 }
 
-document.getElementById('myBtn').addEventListener("mouseover", myFunction);
+function myFunctionChange() {
+  let x = document.getElementById("mySelect").value;
+  document.getElementById("demoSelect").innerHTML = "Wybrałeś: " + x;
+}
+
+
+function myFocusFunction() {
+  document.getElementById("fname").style.backgroundColor = "red";
+}
+
+function myOnKeyDownFunction() {
+  document.getElementById("demoKeyDown").innerHTML = "Wcisnąłeś klawisz wewnątrz pola input";
+}
+
+function myFunctionOnMouseMove(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+  let coor = "Współrzędne: (" + x + "," + y + ")";
+  document.getElementById("demoOnMouseMove").innerHTML = coor;
+}
+
+function clearCoordinates() {
+  document.getElementById("demoOnMouseMove").innerHTML = "";
+}
+
+
+document.getElementById("myBtn").addEventListener("click", displayDate);
+document.getElementById("fname").onfocus = function() {myFocusFunction()};
