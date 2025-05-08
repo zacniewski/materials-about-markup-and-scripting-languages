9442fa6f911a61c2351b62ed6e9efdbdbf62d574
Artur Zacniewski <a.zacniewski@gmail.com>
2025-05-08 11:04:58 +0200

    Added description to lab. 4

--- lab4/js/scripts.js
+++ lab4/js/scripts.js
@@ -1,17 +1,24 @@
+// asynchroniczny JS, konieczny do uzycia funkcji `fetch`
+// można użyć tej opcji, ale należy pamiętać,
+// że w naszym przypadku cała logika powinna być wewnątrz funkcji getCountryData()
+
 async function getCountryData(url) {
     const res = await fetch(url);
     const json_data = await res.json();
 
+    // tu umieszczamy logikę i potem tylko wywołujemy funkcję z odpowiednim parametrem
+    // (w tym przypadku jest to nazwa kraju)
+
     name_of_the_country = json_data[0]["name"]["common"];
-    console.log(name_of_the_country);
+    console.log("The country is: ", name_of_the_country);
 }
 
 const country_url = "https://restcountries.com/v3.1/name/Poland";
 getCountryData(country_url);
 
 // do wycieczek
-// pierwszy obiekt
-console.log(wycieczki[0]);
+// pierwszy obiekt i jego rejon
+console.log("Rejon to: ", wycieczki[0]["rejon"]);
 
 // wszystkie obiekty
 for (i of wycieczki) {
